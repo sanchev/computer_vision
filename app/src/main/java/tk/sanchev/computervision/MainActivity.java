89caa711f876af52035690cc5c1074b039c0c60b
sanchev <sanchev_temp@inbox.ru>
2016-01-28 14:18:55 +0200

    Всё работает как нужно :)
    Теперь необходимо:
    1. Изменить иконку кнопки смены камер.
    2. Добавить кнопку "Сделать снимок" и реализовать ее функционал:
    2.1. Сделать снимок.
    2.2. Шаринг.
    
    :)

--- app/src/main/java/tk/sanchev/computervision/MainActivity.java
+++ app/src/main/java/tk/sanchev/computervision/MainActivity.java
@@ -1,54 +1,46 @@
 package tk.sanchev.computervision;
 
-//import android.content.Context;
-//import android.content.res.AssetManager;
-//import android.graphics.Bitmap;
-//import android.graphics.BitmapFactory;
-//import android.hardware.Camera;
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
 import android.os.Bundle;
-//import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-//import android.util.Log;
 import android.view.SurfaceView;
-//import android.view.View;
+import android.view.View;
 import android.view.WindowManager;
 
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.android.OpenCVLoader;
-//import org.opencv.android.Utils;
-//import org.opencv.core.Core;
-//import org.opencv.core.CvType;
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-//import org.opencv.core.MatOfRect;
-//import org.opencv.core.Point;
-//import org.opencv.core.Rect;
-//import org.opencv.core.Scalar;
-//import org.opencv.core.Size;
-//import org.opencv.imgcodecs.Imgcodecs;
-//import org.opencv.imgproc.Imgproc;
-//import org.opencv.objdetect.CascadeClassifier;
-//import org.opencv.objdetect.Objdetect;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.objdetect.Objdetect;
 
-//import java.io.File;
-//import java.io.FileNotFoundException;
-//import java.io.FileOutputStream;
-//import java.io.IOException;
-//import java.io.InputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity implements CvCameraViewListener2 {
     private CameraBridgeViewBase mOpenCvCameraView;
-    //private int absoluteFaceSize;
-    //private CascadeClassifier faceCascadeOne;
-    //private CascadeClassifier faceCascadeTwo;
-    //private int currentCameraIngex = -1;
-    //private boolean isCurrentCameraFront = false;
-
-    //private Mat matEmoji;
-    //private Mat matMask;
-
-    //AssetManager assetManager;
+    private int absoluteFaceSize;
+    private CascadeClassifier faceCascadeOne;
+    private int currentCameraIngex = -1;
+    private boolean isCurrentCameraFront = false;
+    private Mat matEmoji;
+    private Mat matMask;
+    AssetManager assetManager;
 
     static {
         System.loadLibrary("opencv_java3");
@@ -59,18 +51,14 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        //assetManager = getAssets();
-
-        //faceCascadeOne = newCascadeClassifier("lbpcascade_frontalface.xml");
-        //faceCascadeTwo = newCascadeClassifier("haarcascade_frontalface_alt.xml");
+        assetManager = getAssets();
+        faceCascadeOne = newCascadeClassifier("lbpcascade_frontalface.xml");
 
         setContentView(R.layout.activity_main);
 
         mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.view);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
-        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-
-        /*
         //Инициализация камер START
         int numberOfCameras = Camera.getNumberOfCameras();
         if (numberOfCameras >= 1) {
@@ -81,7 +69,7 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
                 fab.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        currentCameraIngex = currentCameraIngex^1; //bitwise not operation to flip 1 to 0 and vice versa
+                        currentCameraIngex = currentCameraIngex ^ 1; //bitwise not operation to flip 1 to 0 and vice versa
                         checkIsCurrentCameraFront();
                         mOpenCvCameraView.disableView();
                         mOpenCvCameraView.setCameraIndex(currentCameraIngex);
@@ -90,12 +78,11 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
                 });
             }
         }
-        */
-        //mOpenCvCameraView.setCameraIndex(currentCameraIngex);
+        mOpenCvCameraView.setCameraIndex(currentCameraIngex);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(this);
         //Инициализация камер END
-        /*
+
         //Инициализация EMOJI START
         Bitmap bitmapEmoji = BitmapFactory.decodeResource(getResources(), R.drawable.emoji);
         Bitmap bitmapMask = BitmapFactory.decodeResource(getResources(), R.drawable.mask);
@@ -106,19 +93,13 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
         matMask = new Mat(bitmapMask.getWidth(), bitmapMask.getHeight(), CvType.CV_8UC3);
         Utils.bitmapToMat(bitmapMask, matMask);
         //Инициализация EMOJI END
-        */
     }
 
-    /*
     private void checkIsCurrentCameraFront() {
         Camera.CameraInfo currentCameraInfo = new Camera.CameraInfo();
         Camera.getCameraInfo(currentCameraIngex, currentCameraInfo);
-        if ((currentCameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_FRONT))
-            isCurrentCameraFront = true;
-        else
-            isCurrentCameraFront = false;
+        isCurrentCameraFront = (currentCameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_FRONT);
     }
-    */
 
     @Override
     public void onPause() {
@@ -147,76 +128,38 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
     }
 
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
-       //Mat result = inputFrame.rgba();
-        //зеркальное отоброжение входного кадра
-        /*
-        if (isCurrentCameraFront)
+        Mat result = inputFrame.rgba();
+        if (isCurrentCameraFront) //зеркальное отоброжение входного кадра
             Core.flip(result, result, 1);
-        /*
+
         Mat grayFrame = inputFrame.gray();
-        //зеркальное отоброжение входного кадра
-        if (isCurrentCameraFront)
+        if (isCurrentCameraFront) //зеркальное отоброжение входного кадра
             Core.flip(grayFrame, grayFrame, 1);
         Imgproc.equalizeHist(grayFrame, grayFrame);
 
         if (this.absoluteFaceSize == 0) {
             int height = grayFrame.rows();
-            if (Math.round(height * 0.2f) > 0) {
+            if (Math.round(height * 0.2f) > 0)
                 this.absoluteFaceSize = Math.round(height * 0.2f);
-            }
         }
-
 
         MatOfRect faces = new MatOfRect();
-        faceCascadeOne.detectMultiScale(grayFrame, faces, 1.1, 2, 0 | Objdetect.CASCADE_SCALE_IMAGE, new Size(this.absoluteFaceSize, this.absoluteFaceSize), new Size());
+        faceCascadeOne.detectMultiScale(grayFrame, faces, 1.1, 2, Objdetect.CASCADE_SCALE_IMAGE, new Size(this.absoluteFaceSize, this.absoluteFaceSize), new Size());
 
-        /*
-        MatOfRect mouths = new MatOfRect();
-        faceCascadeTwo.detectMultiScale(grayFrame, mouths, 1.1, 2, 0 | Objdetect.CASCADE_SCALE_IMAGE, new Size(this.absoluteFaceSize, this.absoluteFaceSize), new Size());
-        */
-        /*
         Rect[] facesArray = faces.toArray();
-        for (int i = 0; i < facesArray.length; i++) {
-            Rect rect = facesArray[i];
-            //Imgproc.rectangle(result, facesArray[i].tl(), facesArray[i].br(), new Scalar(0, 255, 0, 255), 3);
-
-            int circleRadius = rect.height / 2;
-            double circleCenterX = rect.x + circleRadius;
-            double circleCenterY = rect.y + circleRadius;
-            Point circleCenter = new Point(circleCenterX, circleCenterY);
-            Imgproc.circle(result, circleCenter, circleRadius, new Scalar(0, 255, 0, 255), 3);
-
-            /*
+        for (Rect rect : facesArray) {
             Mat submat = result.submat(rect);
-            Size size = submat.size();
             Mat resizedEmoji = new Mat();
-            Imgproc.resize(matEmoji, resizedEmoji, size);
+            Imgproc.resize(matEmoji, resizedEmoji, submat.size());
             Mat resizedMask = new Mat();
-            Imgproc.resize(matMask, resizedMask, size);
+            Imgproc.resize(matMask, resizedMask, submat.size());
             resizedEmoji.copyTo(submat, resizedMask);
         }
-
-        /*
-        Rect[] mouthsArray = mouths.toArray();
-        for (int i = 0; i < mouthsArray.length; i++){
-            Rect rect = mouthsArray[i];
-            //Imgproc.rectangle(result, mouthsArray[i].tl(), mouthsArray[i].br(), new Scalar(255, 128, 0, 255), 3);
-            int circleRadius = rect.height / 2;
-            double circleCenterX = rect.x + circleRadius;
-            double circleCenterY = rect.y + circleRadius;
-            Point circleCenter = new Point(circleCenterX, circleCenterY);
-            Imgproc.circle(result, circleCenter, circleRadius, new Scalar(255, 128, 0, 255), 3);
-        }
-        */
-        //return result;
-        return inputFrame.gray();
+        return result;
     }
 
-    /*
-    private CascadeClassifier newCascadeClassifier (String file) {
+    private CascadeClassifier newCascadeClassifier(String file) {
         try {
-            // load cascade file from application resources
-
             InputStream is = assetManager.open(file);
             File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
             File mCascadeFile = new File(cascadeDir, file);
@@ -224,33 +167,17 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
 
             byte[] buffer = new byte[4096];
             int bytesRead;
-            while ((bytesRead = is.read(buffer)) != -1) {
+            while ((bytesRead = is.read(buffer)) != -1)
                 os.write(buffer, 0, bytesRead);
-            }
             is.close();
             os.close();
 
             CascadeClassifier result = new CascadeClassifier(mCascadeFile.getAbsolutePath());
-            if (result.empty()) {
-                Log.e("SANCV", "Failed to load cascade classifier");
-            } else
-                Log.i("SANCV", "Loaded cascade classifier from " + mCascadeFile.getAbsolutePath());
-
             cascadeDir.delete();
             return result;
         } catch (IOException e) {
             e.printStackTrace();
-            Log.e("SANCV", "Failed to load cascade. Exception thrown: " + e);
         }
         return null;
     }
-
-    private Mat openFile(String fileName) throws FileNotFoundException {
-        Mat result = Imgcodecs.imread(fileName);
-        if (result.dataAddr() == 0) {
-            throw new FileNotFoundException("Couldn't open file " + fileName);
-        }
-        return result;
-    }
-    */
 }
